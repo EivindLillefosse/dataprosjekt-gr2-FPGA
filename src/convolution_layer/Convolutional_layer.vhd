2c27ff5f8595edbe461f502aa68bf81896c042a2
EivindLillefosse <eivind.lillefosse@gmail.com>
2025-09-19 12:18:12 +0200

    updated convolution layer

--- src/convolution_layer/Convolutional_layer.vhd
+++ src/convolution_layer/Convolutional_layer.vhd
@@ -21,28 +21,37 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use work.types_pkg.all;
+
+
 
 entity conv_layer is
     generic (
-        DATA_WIDTH : integer := 8;
         IMAGE_SIZE : integer := 28;
         KERNEL_SIZE : integer := 3;
-        NUM_FILTERS : integer := 8
+        NUM_FILTERS : integer := 8;
+        STRIDE : integer := 1
     );
     Port ( 
         clk : in STD_LOGIC;
         rst : in STD_LOGIC;
         enable : in STD_LOGIC;
-        input_data : in STD_LOGIC_VECTOR(DATA_WIDTH downto 0);
-        output_data : out STD_LOGIC_VECTOR(DATA_WIDTH downto 0);
+        input_data : in PIXEL;
+        output_data : out OUTPUT_ARRAY(0 to ((IMAGE_SIZE-KERNEL_SIZE)/STRIDE)+1, 
+                                       0 to ((IMAGE_SIZE-KERNEL_SIZE)/STRIDE)+1, 
+                                       0 to 1);
         done : out STD_LOGIC
     );
 end conv_layer;
 
 architecture Behavioral of conv_layer is
-
 begin
-
-
+    -- Architecture logic
+    process(clk, rst)
+    begin 
+        if(rst) then
+            output_data <= (others => (others => (others => '0')));
+        elsif rising_edge(clk) then
+            
 end Behavioral;
 
--- src/types.vhd
+++ src/types.vhd
@@ -3,11 +3,18 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 package types_pkg is
     -- Constants
-    constant KERNEL_SIZE : integer := 3;
+    constant KERNEL_SIZE_1 : integer := 3;
+    constant KERNEL_NUM_1 : integer := 8;
+    constant STRIDE_1       : integer := 1;
+    constant IMAGE_SIZE  : integer := 28;
+
 
     -- Type definitions
     subtype PIXEL is STD_LOGIC_VECTOR(7 downto 0);
-    type IMAGE_VECTOR is array (0 to KERNEL_SIZE-1, 0 to KERNEL_SIZE-1) of PIXEL;
-    type LINE_BUFFER is array (0 to KERNEL_SIZE-2) of PIXEL;
+    type IMAGE_VECTOR is array (0 to KERNEL_SIZE_1-1, 0 to KERNEL_SIZE_1-1) of PIXEL;
+    type LINE_BUFFER is array (0 to KERNEL_SIZE_1-2) of PIXEL;
+    
+    -- Unconstrained array type
+    type OUTPUT_ARRAY is array (natural range <>, natural range <>, natural range <>) of PIXEL;
 
 end package types_pkg;
